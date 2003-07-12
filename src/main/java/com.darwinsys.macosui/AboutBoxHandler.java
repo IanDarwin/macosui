52569ac90574c80cac288fbdd0b1e53687f8c881
idarwin <>
2003-07-12 23:30:20 +0000

    Missed import of JFrame.

--- src/main/java/com.darwinsys.macosui/AboutBoxHandler.java
+++ src/main/java/com.darwinsys.macosui/AboutBoxHandler.java
@@ -1,5 +1,8 @@
 package com.darwinsys.macosui;
 
+import javax.swing.JFrame;
+
+/** Handler for part of MacOS UI extensions */
 public interface AboutHandler {
 	public void showAboutBox(JFrame jf);
 }
