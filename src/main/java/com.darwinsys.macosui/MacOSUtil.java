67ed39573e2afbf71b362764faa9ac9922112608
idarwin <>
2003-07-12 23:27:36 +0000

    Remove detritus remaining due to split from UtilGUI.

--- src/main/java/com.darwinsys.macosui/MacOSUtil.java
+++ src/main/java/com.darwinsys.macosui/MacOSUtil.java
@@ -5,7 +5,7 @@ import java.awt.*;
 /** Utilities for GUI work.
  * @version $Id$
  */
-public class UtilGUI {
+public class MacOSUtil {
 
 	/** Return true if we are running MacOS; needs a few GUI tweaks if so. */
 	public static boolean isMacOS() {
@@ -25,29 +25,5 @@ public class UtilGUI {
 		System.setProperty("apple.laf.useScreenMenuBar",  "true");
 		System.setProperty("com.apple.mrj.application.apple.menu.about.name",
 			appName);
-	}
-
-	/** Centre a Window, Frame, JFrame, Dialog, etc. */
-	public static void centre(Window w) {
-		// After packing a Frame or Dialog, centre it on the screen.
-		Dimension us = w.getSize(), 
-			them = Toolkit.getDefaultToolkit().getScreenSize();
-		int newX = (them.width - us.width) / 2;
-		int newY = (them.height- us.height)/ 2;
-		w.setLocation(newX, newY);
-	}
-
-	/** Center a Window, Frame, JFrame, Dialog, etc., 
-	 * but do it the American Spelling Way :-)
-	 */
-	public static void center(Window w) {
-		UtilGUI.centre(w);
-	}
-
-	/** Maximize a window, the hard way. */
-	public static void maximize(Window w) {
-		Dimension us = w.getSize(),
-			them = Toolkit.getDefaultToolkit().getScreenSize();
-		w.setBounds(0,0, them.width, them.height);
 	}
 }
