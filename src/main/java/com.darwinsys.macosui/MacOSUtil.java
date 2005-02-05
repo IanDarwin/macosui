3dcdc9b3dc7d488f2d0402cd218a5334daf9d2af
idarwin <>
2005-02-05 16:20:21 +0000

    Add a method for running a Mac ".app"application, by finding and starting the executable inside it. Apple probably has this method someplace,but it doesn't appear to be documented.

--- src/main/java/com.darwinsys.macosui/MacOSUtil.java
+++ src/main/java/com.darwinsys.macosui/MacOSUtil.java
@@ -1,5 +1,8 @@
 package com.darwinsys.macosui;
 
+import java.io.File;
+import java.io.IOException;
+
 /** Utilities for GUI work.
  * @version $Id$
  */
@@ -12,8 +15,8 @@ public class MacOSUtil {
 		return System.getProperty("os.name").toLowerCase().startsWith("mac os x");
 	}
 
-	/** Set a few common properties for the given application if
-	 * we are running under MacOS.
+	/** Set a few common properties for the given application if running under MacOS.
+	 * <em>Must be invoked before you construct any Swing components</em>, even JFrame.
 	 * Usage Example:
 	 * <pre>
 	 *	if (MacOSUtil.isMacOS()) {
@@ -27,5 +30,34 @@ public class MacOSUtil {
 		System.setProperty("apple.awt.showGrowBox",  "true");
 		System.setProperty("com.apple.mrj.application.apple.menu.about.name",
 			appName);
+	}
+	
+	private static Runtime theRuntime = Runtime.getRuntime();
+	
+	/** Handle Apple's continuing failure to make their Java implementation's version
+	 * of Runtime.exec() match normal users' behaviour, that is, make Runtime.exec("Foo.app")
+	 * actually run "Foo".
+	 * @see java.lang.Runtime.exec()
+	 * @param command The commands array
+	 * @return The newly-created Process
+	 * @throws IOException If anything goes wrong.
+	 */
+	public Process exec(String[] command) throws IOException {
+		File theFile = new File(command[0]);
+		if (!theFile.exists()) {
+			throw new IOException("Program " + command[0] + " does not exist");
+		}
+		// Cheesy approach. XXX Should parse Info.plist and get the CFBundleExecutable.
+		// This will do for the time being...
+		if (theFile.isDirectory() && theFile.getName().endsWith(".app")) {
+		    File appDirectory = new File(theFile, "Contents/MacOS");
+		    File[] candidates = appDirectory.listFiles();
+		    if (candidates.length != 1) {
+		        throw new IOException("Can't figure out which file to run");
+		    }
+		    theFile = new File(appDirectory, candidates[0].getName());
+		}
+		// now proceed with native exec
+		return theRuntime.exec(command);
 	}
 }
